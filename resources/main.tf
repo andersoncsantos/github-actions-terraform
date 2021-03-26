a127001ae2b6693c0ee353aac63b7433a56029fb
Anderson Santos <anderson.santos@iteris.com.br>
2021-03-26 20:51:19 -0300

    teste github actions

--- resources/main.tf
+++ resources/main.tf
@@ -2,12 +2,12 @@
   region = var.region
 }
 
-resource "aws_s3_bucket" "teste_CI" {
-  bucket = "teste-github-actions-00000001"
-  acl    = "private"
+# resource "aws_s3_bucket" "teste_CI" {
+#   bucket = "teste-github-actions-00000001"
+#   acl    = "private"
 
-  tags = {
-    Name        = ""
-    Environment = ""
-  }
-}
+#   tags = {
+#     Name        = ""
+#     Environment = ""
+#   }
+# }
