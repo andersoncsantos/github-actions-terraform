1e9ef1803c69904fedcd990cdf567dccfa62f8e7
Anderson Santos <anderson.santos@iteris.com.br>
2021-03-30 19:08:59 -0300

    workspaces

--- resources/main.tf
+++ resources/main.tf
@@ -210,6 +210,6 @@ terraform {
     bucket               = "terraform-tfstate-00000001"
     key                  = "terraform.tfstate"
     region               = "us-east-1"
-    workspace_key_prefix = "teste/"
+    workspace_key_prefix = "teste"
   }
 }
