34b8fefdfb4a15b72e3d32792e88277ea1e0b33e
Anderson Santos <anderson.santos@iteris.com.br>
2021-03-30 16:16:39 -0300

    workspaces

--- resources/main.tf
+++ resources/main.tf
@@ -3,15 +3,15 @@
 }
 
 resource "aws_ecr_repository" "faturamento" {
-  name = "faturamento" # Naming my repository
+  name = "faturamento-" + terraform.workspace
 }
 
 resource "aws_ecs_cluster" "faturamento_cluster" {
-  name = "faturamento-cluster" # Naming the cluster
+  name = "faturamento-cluster-" + terraform.workspace
 }
 
 resource "aws_iam_role" "faturamento_task_execution_role" {
-  name               = "faturamento-task-execution-role"
+  name               = "faturamento-task-execution-role-" + terraform.workspace
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
@@ -27,11 +27,11 @@ data "aws_iam_policy_document" "assume_role_policy" {
 }
 
 resource "aws_ecs_task_definition" "faturamento_task" {
-  family                   = "faturamento-task" # Naming our first task
+  family                   = "faturamento-task-" + terraform.workspace
   container_definitions    = <<DEFINITION
   [
     {
-      "name": "faturamento-task",
+      "name": "faturamento-task-${terraform.workspace}",
       "image": "${aws_ecr_repository.faturamento.repository_url}",
       "essential": true,
       "portMappings": [
@@ -45,10 +45,10 @@ resource "aws_ecs_task_definition" "faturamento_task" {
     }
   ]
   DEFINITION
-  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
-  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
-  memory                   = 512         # Specifying the memory our container requires
-  cpu                      = 256         # Specifying the CPU our container requires
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = 512
+  cpu                      = 256
   execution_role_arn       = aws_iam_role.faturamento_task_execution_role.arn
 }
 
@@ -57,20 +57,20 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-# Creating a security group for the load balancer:
+
 resource "aws_security_group" "load_balancer_security_group" {
   ingress {
-    from_port   = 80 # Allowing traffic in from port 80
+    from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0             # Allowing any incoming port
-    to_port     = 0             # Allowing any outgoing port
-    protocol    = "-1"          # Allowing any outgoing protocol 
-    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -79,23 +79,21 @@ resource "aws_security_group" "service_security_group" {
     from_port = 0
     to_port   = 0
     protocol  = "-1"
-    # Only allowing traffic in from the load balancer security group
+
     security_groups = [aws_security_group.load_balancer_security_group.id]
   }
 
   egress {
-    from_port   = 0             # Allowing any incoming port
-    to_port     = 0             # Allowing any outgoing port
-    protocol    = "-1"          # Allowing any outgoing protocol 
-    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-# Providing a reference to our default VPC
 resource "aws_default_vpc" "default_vpc" {
 }
 
-# Providing a reference to our default subnets
 resource "aws_default_subnet" "default_subnet_a" {
   availability_zone = "us-east-1a"
 }
@@ -109,23 +107,23 @@ resource "aws_default_subnet" "default_subnet_c" {
 }
 
 resource "aws_alb" "application_load_balancer" {
-  name               = "faturamento-lb" # Naming our load balancer
+  name               = "faturamento-lb-" + terraform.workspace
   load_balancer_type = "application"
-  subnets = [ # Referencing the default subnets
+  subnets = [
     aws_default_subnet.default_subnet_a.id,
     aws_default_subnet.default_subnet_b.id,
     aws_default_subnet.default_subnet_c.id
   ]
-  # Referencing the security group
+
   security_groups = [aws_security_group.load_balancer_security_group.id]
 }
 
 resource "aws_lb_target_group" "faturamento_target_group" {
-  name        = "faturamento-target-group"
+  name        = "faturamento-target-group-" + terraform.workspace
   port        = 80
   protocol    = "HTTP"
   target_type = "ip"
-  vpc_id      = aws_default_vpc.default_vpc.id # Referencing the default VPC
+  vpc_id      = aws_default_vpc.default_vpc.id
   health_check {
     matcher = "200,301,302"
     path    = "/"
@@ -133,12 +131,12 @@ resource "aws_lb_target_group" "faturamento_target_group" {
 }
 
 resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_alb.application_load_balancer.arn # Referencing our load balancer
+  load_balancer_arn = aws_alb.application_load_balancer.arn
   port              = "80"
   protocol          = "HTTP"
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.faturamento_target_group.arn # Referencing our tagrte group
+    target_group_arn = aws_lb_target_group.faturamento_target_group.arn
   }
 }
 
@@ -146,16 +144,16 @@ resource "aws_ecs_service" "faturamento_service" {
   depends_on = [
     aws_alb.application_load_balancer
   ]
-  name            = "faturamento-service"                        # Naming our first service
-  cluster         = aws_ecs_cluster.faturamento_cluster.id       # Referencing our created Cluster
-  task_definition = aws_ecs_task_definition.faturamento_task.arn # Referencing the task our service will spin up
+  name            = "faturamento-service-" + terraform.workspace
+  cluster         = aws_ecs_cluster.faturamento_cluster.id
+  task_definition = aws_ecs_task_definition.faturamento_task.arn
   launch_type     = "FARGATE"
-  desired_count   = 1 # Setting the number of containers we want deployed to 1
+  desired_count   = 1
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.faturamento_target_group.arn # Referencing our target group
+    target_group_arn = aws_lb_target_group.faturamento_target_group.arn
     container_name   = aws_ecs_task_definition.faturamento_task.family
-    container_port   = 3000 # Specifying the container port
+    container_port   = 3000
   }
 
   network_configuration {
@@ -164,40 +162,14 @@ resource "aws_ecs_service" "faturamento_service" {
       aws_default_subnet.default_subnet_b.id,
       aws_default_subnet.default_subnet_c.id
     ]
-    assign_public_ip = true # Providing our containers with public IPs
+    assign_public_ip = true
   }
 }
 
-
-# provider "aws" {
-#   region = var.region
-# }
-
-# resource "aws_s3_bucket" "teste_CI" {
-#   bucket = "teste-github-actions-00000001"
-#   acl    = "private"
-
-#   tags = {
-#     Name        = ""
-#     Environment = ""
-#   }
-# }
-
-# resource "aws_s3_bucket" "tfstate" {
-#   bucket        = "terraform-tfstate-00000001"
-#   acl           = "private"
-#   force_destroy = true
-
-#   tags = {
-#     Name        = ""
-#     Environment = ""
-#   }
-# }
-
 terraform {
   backend "s3" {
-    bucket = "terraform-tfstate-00000001"
-    key    = "terraform.tfstate"
+    bucket = "omega-terraform-tfstate"
+    key    = "faturamento-back-${terraform.workspace}.tfstate"
     region = "us-east-1"
   }
 }
