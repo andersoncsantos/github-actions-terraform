fe21404ba4ee28d0518a719e083fca3bc059bdf7
Anderson Santos <anderson.santos@iteris.com.br>
2021-03-30 18:51:59 -0300

    workspaces

--- resources/main.tf
+++ resources/main.tf
@@ -207,8 +207,8 @@ resource "aws_ecs_service" "faturamento_service" {
 
 terraform {
   backend "s3" {
-    bucket = "terraform-tfstate-00000001"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
+    bucket               = "terraform-tfstate-00000001"
+    workspace_key_prefix = "terraform.tfstate"
+    region               = "us-east-1"
   }
 }
